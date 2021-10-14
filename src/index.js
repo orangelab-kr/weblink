7ecfb36845b7d8f8c01b20fadb47a59d7f46e275
bakjonghun <bakjonghun@hikick.kr>
2021-10-14 14:25:40 +0900

    add: devOps (github actions)

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Reset } from 'styled-reset';
-import { RequiredAuth, Setting } from '.';
-import { AuthAuthorize } from './pages/auth/Authorize';
+import { AuthAuthorize, RequiredAuth, Setting } from '.';
 
 export * from './components';
 export * from './pages';
 export * from './tools';
 
-export const baseURL = 'https://coreservice.staging.hikick.kr/v1';
+export const baseURL =
+  window.location.host === 'weblink.hikick.kr'
+    ? 'https://coreservice.hikick.kr/v1'
+    : 'https://coreservice.staging.hikick.kr/v1';
 
 ReactDOM.render(
   <React.StrictMode>
