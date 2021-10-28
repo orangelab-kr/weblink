bb27ca9fbfeda02346be0412bcf2c08fca86904e
bakjonghun <bakjonghun@hikick.kr>
2021-10-28 14:03:57 +0900

    add: optimize to webview

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import { Reset } from 'styled-reset';
 import { AuthAuthorize, RequiredAuth, Setting } from '.';
 
@@ -13,23 +14,33 @@ export const baseURL =
     ? 'https://coreservice.hikick.kr/v1'
     : 'https://coreservice.staging.hikick.kr/v1';
 
+const GlobalStyle = styled.div`
+  * {
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+    -webkit-touch-callout: none;
+  }
+`;
+
 ReactDOM.render(
   <React.StrictMode>
     <Reset />
-    <BrowserRouter>
-      <Switch>
-        <Route path="/auth/authorize">
-          <AuthAuthorize />
-        </Route>
-        <Route path="/">
-          <RequiredAuth>
-            <Route path="/setting">
-              <Setting />
-            </Route>
-          </RequiredAuth>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <GlobalStyle>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/auth/authorize">
+            <AuthAuthorize />
+          </Route>
+          <Route path="/">
+            <RequiredAuth>
+              <Route path="/setting">
+                <Setting />
+              </Route>
+            </RequiredAuth>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </GlobalStyle>
   </React.StrictMode>,
   document.getElementById('root')
 );
